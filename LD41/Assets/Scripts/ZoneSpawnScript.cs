3b06f953223779e95f9688bb5f6c0b401338b994
lmagitem <lukemagitem@gmail.com>
2018-04-22 17:03:47 +0200

    Initial

--- LD41/Assets/Scripts/ZoneSpawnScript.cs
+++ LD41/Assets/Scripts/ZoneSpawnScript.cs
@@ -8,11 +8,11 @@ public class ZoneSpawnScript : MonoBehaviour {
 	void Start () {
         GameObject prefab = Resources.Load("CarPlayer") as GameObject;
         GameObject gamemanager = GameObject.FindWithTag("GameManager");
-        for(int i = 0; i < gamemanager.GetComponent<GameManagerScript>().playerCount; i++)
+        /*for(int i = 0; i < gamemanager.GetComponent<GameManagerScript>().playerCount; i++)
         {
             Vector3 tempTransform = gameObject.transform.position; 
             GameObject temp = Instantiate(prefab, new Vector3(, gameObject.transform.localRotation, null);
-        }
+        }*/
 
     }
 	
